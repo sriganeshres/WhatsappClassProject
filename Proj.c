56c435c42f8adf4a71d5ba0ada437fe7f6e9b3c1
sriganeshres <sriganeshthota12345@gmail.com>
2023-05-17 22:27:49 +0530

    ICS Project Files

---
+++ Proj.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(int argc, char** argv)
+{
+    int i = 2;
+    while(i < argc)
+    {
+        const char * filename = (const char*)argv[1];
+        FILE* src; 
+        int num[1000] = {0};                     
+        src = fopen(filename, "r"); 
+        if(src == NULL)
+            printf("NOT found");
+        while(fgetc(src) != EOF)
+        {
+            char line[10000];
+            const char * name = (const char*)argv[i];
+            fgets(line, 1000, src);
+            char* q = strstr(line, name);
+            if(q != NULL)
+            {
+                num[i]++;
+            }  
+        }
+        printf("%s has interacted %d in this group\n", argv[i], num[i]);
+        i++;
+        fclose(src);
+        free(src);
+    }
+}
